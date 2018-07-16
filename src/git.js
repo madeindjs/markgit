e3e9e688e5ba75633faa12887a38312953b455c0
Rousseau Alexandre <contact@rousseau-alexandre.Fr>
2018-07-16 22:41:01 +0200

    Add electron

--- src/git.js
+++ src/git.js
@@ -1,33 +1,62 @@
 const fs = require('fs')
 const path = require('path')
+const SimpleMDE = require('./node_modules/simplemde/dist/simplemde.min.js')
 
 
-const mkdirSync = function (dirPath) {
-    try {
-        fs.mkdirSync(dirPath)
-    } catch (err) {
-        if (err.code !== 'EEXIST') throw err
-    }
-    
-
-    fs.writeFile(dirPath + 'touch.log', "Hey there!", function(err) {
-        if(err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    }); 
-}
+const AUTO_SAVE_TIME = 2000
+const NOTE_PATH = '/tmp/test/'
 
 
+let oldValue = ''
 
-mkdirSync('/tmp/test/')
+// create editor
+let simplemde = new SimpleMDE({
+    element: document.getElementById("editor"),
+});
 
-require('simple-git')('/tmp/test/')
+// create repository
+let repository = require('simple-git')(NOTE_PATH)
     .init()
     // TODO: Setup this in settings
     .addConfig('user.name', 'Some One')
     .addConfig('user.email', 'some@one.com')
-    .add('./*')
-    .commit("first commit!")
-    // .addRemote('origin', 'https://github.com/user/repo.git')
-    // .push('origin', 'master');
+    
+
+// create the given directory
+const mkdirSync = function () {
+    try {
+        fs.mkdirSync(NOTE_PATH)
+    } catch (err) {
+        if (err.code !== 'EEXIST') throw err
+    }
+}
+
+
+// write file & commit
+function save(value) {
+    fs.writeFile(NOTE_PATH + 'touch.log', value, function(err) {
+        if(err) {
+            return console.log(err)
+        }
+
+        repository.add('./*').commit("Automatic save")
+        
+        oldValue = value
+    });
+}
+
+// save the current mde opened & commit if necessary
+function automaticSave() {
+    setTimeout(() => {
+        let newValue = simplemde.value()
+        // skip this method if value not changed
+        if (oldValue != newValue) {
+            save(newValue)
+        }
+        return automaticSave()
+    }, AUTO_SAVE_TIME);
+}
+
+
+mkdirSync(NOTE_PATH)
+automaticSave()
