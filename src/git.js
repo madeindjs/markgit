1aa57714fa63f129f5000a528de586a4d534a5d6
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2018-07-17 13:02:55 +0200

    Organize code

--- src/git.js
+++ src/git.js
@@ -1,11 +1,16 @@
 const fs = require('fs')
 const path = require('path')
 const SimpleMDE = require('./node_modules/simplemde/dist/simplemde.min.js')
+const Repository = require('./src/repository.js')
 
 
 const AUTO_SAVE_TIME = 2000
 const NOTE_PATH = '/tmp/test/'
 const NOTE_FILE = NOTE_PATH + 'touch.log'
+
+
+
+let repo = new Repository('/tmp/test/')
 
 
 let oldValue = ''
@@ -15,45 +20,13 @@ let simplemde = new SimpleMDE({
     element: document.getElementById("editor"),
 });
 
-// create repository
-let repository = require('simple-git')(NOTE_PATH)
-    .init()
-    // TODO: Setup this in settings
-    .addConfig('user.name', 'Some One')
-    .addConfig('user.email', 'some@one.com')
-    
-
-// create the given directory
-const mkdirSync = function () {
-    try {
-        fs.mkdirSync(NOTE_PATH)
-    } catch (err) {
-        if (err.code !== 'EEXIST') throw err
-    }
-}
-
-
-// write file & commit
-function save(value) {
-    fs.writeFile(NOTE_FILE, value, function(err) {
-        if(err) {
-            return alert(err)
-        }
-
-        repository.add('./*').commit("Automatic save")
-        console.log('Commit created')
-
-        oldValue = value
-    });
-}
-
 // save the current mde opened & commit if necessary
 function automaticSave() {
     setTimeout(() => {
         let newValue = simplemde.value()
         // skip this method if value not changed
         if (oldValue != newValue) {
-            save(newValue)
+            repo.save(newValue)
         }
         return automaticSave()
     }, AUTO_SAVE_TIME);
@@ -62,11 +35,10 @@ function automaticSave() {
 
 // setup everything
 function init() {
-    // create folder if not exist
-    mkdirSync(NOTE_PATH)
-
+    // create file if not exists
+    fs.closeSync(fs.openSync(NOTE_FILE, 'a'));
     // get value from file
-    fs.readFile(NOTE_FILE, 'utf8', function (err,data) {
+    fs.readFile(NOTE_FILE, 'utf8', function (err, data) {
         if (err) {
             return console.log(err);
         }
---
+++ src/repository.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+
+module.exports = class Repository {
+
+
+
+
+    constructor(path){
+        this.path = path
+        this.createDirectory()
+
+        this.simpleGit = require('simple-git')(this.path)
+            .init()
+            // TODO: Setup this in settings
+            .addConfig('user.name', 'Some One')
+            .addConfig('user.email', 'some@one.com')
+    }
+
+    // write file & commit
+    save(value) {
+
+        let simpleGit = this.simpleGit
+
+        fs.writeFile(NOTE_FILE, value, function(err) {
+            if(err) {
+                return alert(err)
+            }
+
+            simpleGit.add('./*').commit("Automatic save", () => {
+                console.log('Commit created')
+                oldValue = value
+            })
+
+        });
+    }
+
+
+    // create the given directory
+    createDirectory() {
+        try {
+            fs.mkdirSync(this.path)
+        } catch (err) {
+            if (err.code !== 'EEXIST') throw err
+        }
+    }
+
+
+}
